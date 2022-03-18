617454bdb578820a49b40c415e74d8f7c460da9c
shatabd <shatabdi.shah16@gmail.com>
2022-03-19 00:08:21 +0545

    Activity lifecycle Challenge completed

---
+++ app/src/main/java/com/example/activitylifecyclechallenge/SecondActivity.java
@@ -0,0 +1,25 @@
+package com.example.activitylifecyclechallenge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+//add the item
+        public void addItem(View view) {
+            String item = ((Button)view).getText().toString();
+            Intent itemIntent = new Intent();
+            itemIntent.putExtra(EXTRA_REPLY, item);
+            setResult(RESULT_OK, itemIntent);
+            finish();
+    }
+}
